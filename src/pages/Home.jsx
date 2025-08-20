80fcb8e8cff64800036670aa3a6df71da12e28c5
gabriel1001g <gabrielsg2000@gmail.com>
2025-08-20 18:34:36 -0300

    implementando Swiper na Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,12 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/swiper-bundle.css';
+import armario from '../assets/img/armario.jpg';
+import armario2 from '../assets/img/armario2.jpg';
+import armario3 from '../assets/img/armario3.jpg';
 
 export default function Home() {
+
+  SwiperCore.use([Navigation, Pagination, Autoplay]);
 
     return(
     <div className="flex flex-col min-h-screen bg-[#03033D] text-white relative justify-center items-center">
@@ -7,16 +14,30 @@ export default function Home() {
             <h1 className="text-2xl font-semibold text-white mb-4">Bem Vindo!</h1>
             <h3 className="text-1 font-semibold text-white">Recomendamos para você</h3>
             <h3 className="text-1 font-semibold text-white mb-4">Novas locações</h3>
-            <div className="flex flex-col flex-1 px-6">
-
+           
+            <Swiper modules={[Navigation, Pagination, Autoplay]}  spaceBetween={16}  slidesPerView={1} 
+            navigation pagination={{ clickable: true }} loop autoplay={{delay: 3000,disableOnInteraction: false,pauseOnMouseEnter: true}} >
+            
+          <SwiperSlide>
+            <div className= "bg-blue-600 text-white rounded-2xl p-20 flex justify-center items-center mb-20" >
+            <img src={armario} alt="Recomendado" className="absolute inset-0 w-full h-full object-cover" /> 
+          
             </div>
-
+          </SwiperSlide>
+          <SwiperSlide>
             <div className="bg-blue-600 text-white rounded-2xl p-20 flex justify-center items-center mb-20">
-            <span className="text-lg">Imagem</span>
-          </div>
-          <div className="bg-blue-600 text-white rounded-2xl p-20 flex justify-center items-center mb-20">
-            <span className="text-lg">Imagem</span>
-          </div>
+              <img src={armario2} alt="Recomendado" className="absolute inset-0 w-full h-full object-cover" /> 
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="bg-blue-600 text-white rounded-2xl p-20 flex justify-center items-center mb-20">
+              <img src={armario3} alt="Recomendado" className="absolute inset-0 w-full h-full object-cover" /> 
+            </div>
+          </SwiperSlide>
+          </Swiper>
+              
+
+           
 
         </div>
     </div>
